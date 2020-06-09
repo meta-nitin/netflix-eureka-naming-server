43f8741de0a925fb7b3ab22cccaae19ccc16b6aa
Nitin <knacky.nitin@gmail.com>
2020-06-10 01:44:16 +0530

    initial commit

---
+++ netflix-eureka-naming-server/src/test/java/org/self/netflixeurekanamingserver/NetflixEurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.self.netflixeurekanamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixEurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
